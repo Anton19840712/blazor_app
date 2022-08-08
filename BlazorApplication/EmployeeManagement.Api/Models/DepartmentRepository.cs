08de3ec1d70010287a2eaf1b721a5a061a84c30a
Anton Anatolievich Gridushko <anton.griduyshko@yahoo>
2022-08-08 21:17:12 +0300

    Add department repo and its implementation.

---
+++ BlazorApplication/EmployeeManagement.Api/Models/DepartmentRepository.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using EmployeeManagement.Models;
+
+namespace EmployeeManagement.Api.Models
+{
+	public class DepartmentRepository : IDepartmentRepository
+	{
+		private readonly AppDbContext _appDbContext;
+
+		public DepartmentRepository(AppDbContext appDbContext)
+		{
+			this._appDbContext = appDbContext;
+		}
+
+		public Department GetDepartment(int departmentId)
+		{
+			return _appDbContext.Departments
+				.FirstOrDefault(d => d.DepartmentId == departmentId);
+		}
+
+		public IEnumerable<Department> GetDepartments()
+		{
+			return _appDbContext.Departments;
+		}
+	}
+}
