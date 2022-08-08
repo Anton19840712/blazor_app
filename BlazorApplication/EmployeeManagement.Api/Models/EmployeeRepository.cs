734e041a686cdf63607622521fc1f1c71abc08f9
Anton Anatolievich Gridushko <anton.griduyshko@yahoo>
2022-08-08 20:41:38 +0300

    Add employee repository abstraction and implemet contract.

---
+++ BlazorApplication/EmployeeManagement.Api/Models/EmployeeRepository.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using EmployeeManagement.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeManagement.Api.Models
+{
+	public class EmployeeRepository : IEmployeeRepository
+	{
+		private readonly AppDbContext _appDbContext;
+
+		public EmployeeRepository(AppDbContext appDbContext)
+		{
+			_appDbContext = appDbContext;
+		}
+
+		public async Task<IEnumerable<Employee>> GetEmployeesAsync()
+		{
+			return await _appDbContext.Employees.ToListAsync();
+		}
+
+		public async Task<Employee> GetEmployeeAsync(int employeeId)
+		{
+			return await _appDbContext.Employees
+				.FirstOrDefaultAsync(e => e.EmployeeId == employeeId);
+		}
+
+		public async Task<Employee> AddEmployeeAsync(Employee employee)
+		{
+			var result = await _appDbContext.Employees.AddAsync(employee);
+			await _appDbContext.SaveChangesAsync();
+			return result.Entity;
+		}
+
+		public async Task<Employee> UpdateEmployeeAsync(Employee employee)
+		{
+			var result = await _appDbContext.Employees
+				.FirstOrDefaultAsync(e => e.EmployeeId == employee.EmployeeId);
+
+			if (result != null)
+			{
+				result.FirstName = employee.FirstName;
+				result.LastName = employee.LastName;
+				result.Email = employee.Email;
+				result.DateOfBirth = employee.DateOfBirth;
+				result.Gender = employee.Gender;
+				result.DepartmentId = employee.DepartmentId;
+				result.PhotoPath = employee.PhotoPath;
+
+				await _appDbContext.SaveChangesAsync();
+
+				return result;
+			}
+
+			return null;
+		}
+
+		public async void DeleteEmployeeAsync(int employeeId)
+		{
+			var result = await _appDbContext.Employees
+				.FirstOrDefaultAsync(e => e.EmployeeId == employeeId);
+			if (result != null)
+			{
+				_appDbContext.Employees.Remove(result);
+				await _appDbContext.SaveChangesAsync();
+			}
+		}
+	}
+}
---
+++ BlazorApplication/EmployeeManagement.Api/Models/IEmployeeRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using EmployeeManagement.Models;
+
+namespace EmployeeManagement.Api.Models
+{
+	public interface IEmployeeRepository
+	{
+		Task<IEnumerable<Employee>> GetEmployeesAsync();
+		Task<Employee> GetEmployeeAsync(int employeeId);
+		Task<Employee> AddEmployeeAsync(Employee employee);
+		Task<Employee> UpdateEmployeeAsync(Employee employee);
+		void DeleteEmployeeAsync(int employeeId);
+	}
+}
